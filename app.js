abe9eed0d064486035527a7bba628417c99d3ea6
M. KALEEMULLAH KHAN QURESHI <kaleemullahqurashi29@gmail.com>
2023-10-14 23:30:35 +0500

    deliver to github

---
+++ app.js
@@ -0,0 +1,109 @@
+
+//Alerts 
+//Q:1
+var q1="Error!PLease Enter A Valid Password"
+alert(q1)
+
+
+
+//Q:2
+var name="Error! Please Enter a Valid Password";
+
+alert(name)
+
+
+var name="Welcome to JS Land... \n Happy Coding"
+
+alert(name)
+
+
+//Q:3
+var welcome="Welcome To JS Land..."
+alert(welcome)
+
+var more="Happy Coding!\n Prevent This Page From Creating Additional Dialogs"
+alert(more)
+
+//Q:5
+var hello="Hello ... I can run JS ThroughMy Web Browser's console"
+
+console.log(hello)
+
+// var for strings 
+//Q:2
+var myName="kaleemullah"
+alert(myName)
+
+//Q:3
+ var message="Hello World"
+ alert(message)
+
+//Q:4
+var myName="kaleemullah"
+alert(myName)
+
+//Q:4
+var age="21 years old"
+alert(age)
+
+
+//Q:5
+var course="Certified web Developer"
+alert(course)
+
+//Q:6
+var food="pizza \n pizz \n piz \n pi \n p"
+alert(food)
+
+//Q:7
+var myEmail="My email address is kaleemullahqurashi29@gmail.com"
+alert(myEmail)
+
+//Q:8
+var Book="I am trying to learn from the Book A Smarter \n Way to learn Javascript"
+alert(Book)
+
+//Q:9
+var line="Yah! I can write HTML content through JavaScript"
+document.write(line)
+
+var design="--------------!#$%$#!---------------"
+
+alert(design)
+
+// Variables for Numbers
+//Q:1
+
+var age="my age is 21"
+
+alert(age)
+
+var visit="you have visit 6 times this site"
+
+alert(visit)
+
+var birthYear="my birth year is 2002"
+
+alert(birthYear)
+
+
+var visitorName=" \n kaleem"
+document.write(visitorName)
+var productTitle="ordered T-shirt(L)"
+document.write(productTitle)
+var clotingStoreName="on chase up store"
+document.write(clotingStoreName)
+
+
+let a=1,b=2,c=3;
+
+//legal Variables name
+
+
+var legalName="myVariable \n  counter \n _privateVar \n $price \n CamelCaseVariableName"
+document.write(legalName)
+
+var illegalName="123abc \n my-variable \n let \n break \n my variable"
+
+document.write(illegalName)
+
